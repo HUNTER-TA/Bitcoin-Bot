ae0cc32b506ca6c09dbc7295c33236877a6cc3f4
HUNTER <hunterta_0901@yahoo.co.jp>
2021-02-22 01:50:42 +0900

    練習5 - 成行買い注文処理追加

--- Bitcoin-Bot/Form1.Designer.cs
+++ Bitcoin-Bot/Form1.Designer.cs
@@ -33,6 +33,12 @@ namespace Bitcoin_Bot
             this.button1 = new System.Windows.Forms.Button();
             this.labelPrice = new System.Windows.Forms.Label();
             this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.groupBoxMarketOrder = new System.Windows.Forms.GroupBox();
+            this.labelMarketOrder = new System.Windows.Forms.Label();
+            this.numericUpDownMarketOrder = new System.Windows.Forms.NumericUpDown();
+            this.buttonMarketOrder = new System.Windows.Forms.Button();
+            this.groupBoxMarketOrder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMarketOrder)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,7 +52,7 @@ namespace Bitcoin_Bot
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(348, 61);
+            this.button1.Location = new System.Drawing.Point(659, 74);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 45);
             this.button1.TabIndex = 1;
@@ -72,17 +78,72 @@ namespace Bitcoin_Bot
             this.textBoxPrice.Size = new System.Drawing.Size(151, 23);
             this.textBoxPrice.TabIndex = 3;
             // 
+            // groupBoxMarketOrder
+            // 
+            this.groupBoxMarketOrder.Controls.Add(this.buttonMarketOrder);
+            this.groupBoxMarketOrder.Controls.Add(this.numericUpDownMarketOrder);
+            this.groupBoxMarketOrder.Controls.Add(this.labelMarketOrder);
+            this.groupBoxMarketOrder.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBoxMarketOrder.Location = new System.Drawing.Point(280, 26);
+            this.groupBoxMarketOrder.Name = "groupBoxMarketOrder";
+            this.groupBoxMarketOrder.Size = new System.Drawing.Size(227, 100);
+            this.groupBoxMarketOrder.TabIndex = 4;
+            this.groupBoxMarketOrder.TabStop = false;
+            this.groupBoxMarketOrder.Text = "成行注文";
+            // 
+            // labelMarketOrder
+            // 
+            this.labelMarketOrder.AutoSize = true;
+            this.labelMarketOrder.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelMarketOrder.Location = new System.Drawing.Point(16, 33);
+            this.labelMarketOrder.Name = "labelMarketOrder";
+            this.labelMarketOrder.Size = new System.Drawing.Size(72, 16);
+            this.labelMarketOrder.TabIndex = 0;
+            this.labelMarketOrder.Text = "注文枚数";
+            // 
+            // numericUpDownMarketOrder
+            // 
+            this.numericUpDownMarketOrder.DecimalPlaces = 3;
+            this.numericUpDownMarketOrder.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDownMarketOrder.Location = new System.Drawing.Point(95, 33);
+            this.numericUpDownMarketOrder.Name = "numericUpDownMarketOrder";
+            this.numericUpDownMarketOrder.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownMarketOrder.TabIndex = 1;
+            this.numericUpDownMarketOrder.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            // 
+            // buttonMarketOrder
+            // 
+            this.buttonMarketOrder.Location = new System.Drawing.Point(140, 62);
+            this.buttonMarketOrder.Name = "buttonMarketOrder";
+            this.buttonMarketOrder.Size = new System.Drawing.Size(75, 23);
+            this.buttonMarketOrder.TabIndex = 2;
+            this.buttonMarketOrder.Text = "発注";
+            this.buttonMarketOrder.UseVisualStyleBackColor = true;
+            this.buttonMarketOrder.Click += new System.EventHandler(this.buttonMarketOrder_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxMarketOrder);
             this.Controls.Add(this.textBoxPrice);
             this.Controls.Add(this.labelPrice);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBoxMarketOrder.ResumeLayout(false);
+            this.groupBoxMarketOrder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMarketOrder)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +154,10 @@ namespace Bitcoin_Bot
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelPrice;
         private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.GroupBox groupBoxMarketOrder;
+        private System.Windows.Forms.Button buttonMarketOrder;
+        private System.Windows.Forms.NumericUpDown numericUpDownMarketOrder;
+        private System.Windows.Forms.Label labelMarketOrder;
     }
 }
 
--- Bitcoin-Bot/Form1.cs
+++ Bitcoin-Bot/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using System.Security.Cryptography;
 
 namespace Bitcoin_Bot
 {
@@ -37,6 +38,12 @@ namespace Bitcoin_Bot
             public double volume_by_product { get; set; }
         }
 
+        ///v1/me/sendchildorder で受け取るレスポンスの型を定義
+        public class JsonSendChildOrder
+        {
+            public string child_order_acceptance_id;
+        }
+
         //最終取引価格を TextBox に出力する
         public async Task GetCurrentBtcFxPrice()
         {
@@ -61,6 +68,72 @@ namespace Bitcoin_Bot
             }
         }
 
+        static readonly string apiKey = "{{ YOUR API KEY }}";
+        static readonly string apiSecret = "{{ YOUR API SECRET }}";
+
+        public async Task<string> MakeMarketOrder()
+        {
+            var method = "POST";
+            var path = "/v1/me/sendchildorder";
+            var query = "";
+            var body = @"{
+              ""product_code"": ""FX_BTC_JPY"",
+              ""child_order_type"": ""MARKET"",
+              ""side"": ""BUY"",
+              ""price"": 0,
+              ""size"": " + numericUpDownMarketOrder.Value + @",  
+              ""minute_to_expire"": 43200,
+              ""time_in_force"": ""GTC""
+              }";
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage(new HttpMethod(method), path + query))
+            using (var content = new StringContent(body))
+            {
+                client.BaseAddress = endpointUri;
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                request.Content = content;
+
+                var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                var data = timestamp + method + path + query + body;
+                var hash = SignWithHMACSHA256(data, apiSecret);
+                request.Headers.Add("ACCESS-KEY", apiKey);
+                request.Headers.Add("ACCESS-TIMESTAMP", timestamp);
+                request.Headers.Add("ACCESS-SIGN", hash);
+
+                var message = await client.SendAsync(request);
+
+                var response = await message.Content.ReadAsStringAsync();
+                if (response == "[]")
+                {
+                    textBox1.AppendText("  Error: GetOrderInformationWithID() returning ERROR_NO_RESPONSE" + System.Environment.NewLine);
+                    return "ERROR_NO_RESPONSE";
+                }
+
+                return response;
+            }
+        }
+
+        static string SignWithHMACSHA256(string data, string secret)
+        {
+            using (var encoder = new HMACSHA256(Encoding.UTF8.GetBytes(secret)))
+            {
+                var hash = encoder.ComputeHash(Encoding.UTF8.GetBytes(data));
+                return ToHexString(hash);
+            }
+        }
+
+        static string ToHexString(byte[] bytes)
+        {
+            var sb = new StringBuilder(bytes.Length * 2);
+            foreach (var b in bytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+
         public Form1()
         {
             InitializeComponent();
@@ -69,7 +142,32 @@ namespace Bitcoin_Bot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void buttonMarketOrder_Click(object sender, EventArgs e)
+        {
+            Task job1 = RunThisWhenButtonMarketOrderIsClicked();
+        }
+
+        public async Task RunThisWhenButtonMarketOrderIsClicked()
+        {
+            textBox1.AppendText("RunThisWhenButtonMarketOrderIsClicked():IN" + System.Environment.NewLine);
+
+            //MakeMarketOrder() で成行注文を発行
+            string ResponseMakeMarketOrder = await MakeMarketOrder();
+            textBox1.AppendText(ResponseMakeMarketOrder + System.Environment.NewLine);
+
+            //以下のようなレスポンスの文字列から値だけを取り出す
+            //{ "child_order_acceptance_id":"JRF20180314-102635-135926"}
+            var DesirializedResponse = JsonConvert.DeserializeObject<JsonSendChildOrder>(ResponseMakeMarketOrder);
+            var ChildOrderAcceptanceID = DesirializedResponse.child_order_acceptance_id;
+
+            //処理をした旨を通知
+            textBox1.AppendText("--------------------------" + System.Environment.NewLine);
+            textBox1.AppendText("Market Order made: " + numericUpDownMarketOrder.Value + System.Environment.NewLine);
+            textBox1.AppendText(ChildOrderAcceptanceID + System.Environment.NewLine);
+            textBox1.AppendText("--------------------------" + System.Environment.NewLine);
         }
     }
 }
